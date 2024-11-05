9112cfe7a9167208c87f970aaeb1870151b02b04
ZeZeCheese <145989185+ZeZeCheese@users.noreply.github.com>
2024-11-05 12:22:48 +0100

    Add files via upload

---
+++ Notitie/Notite.js
@@ -0,0 +1,13 @@
+// Get element first is ID, innerhtml output het in html
+
+document.getElementById("first").innerHTML = 5 * 5;
+
+window.alert("JavaScript Alert");
+
+// Veranderd de tekst
+document.getElementById('h1').textContent = 'Updated Header';
+document.getElementById('p1').textContent = 'Updated paragraph';
+
+// Veranderd de styling (kleur en border)
+p.style.color = "blue";
+p.style.border = "solid";
